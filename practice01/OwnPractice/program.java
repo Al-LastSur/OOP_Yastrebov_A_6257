a1839e230c5172a1b6f39cd0192d481b716daf72
Alex.LastSurvivour <last.sur.90@gmail.com>
2024-06-06 17:24:52 +0500

    Added some check for program file

--- practice01/OwnPractice/program.java
+++ practice01/OwnPractice/program.java
@@ -6,6 +6,10 @@ public class program {
         Product product2 = new BottleOfWater("2", 200, 450);
         Product product3 = new BottleOfWater("3", 300, 350);
         Product product4 = new BottleOfWater("4", 400, 250);
+        Product product5 = new HotDrink("5", 500, 200, 35);
+        Product product6 = new HotDrink("6", 500, 200, 15);
+        Product product7 = new HotDrink("7", 500, 200, 40);
+        Product product8 = new HotDrink("8", 500, 200, 80);
 
         VendingMachine vendingMachine = new VendingMachineBottleOfWater();
         vendingMachine.addProduct(product1);
@@ -13,6 +17,17 @@ public class program {
         vendingMachine.addProduct(product3);
         vendingMachine.addProduct(product4);
 
-        // System.out.println(vendingMachine.getProduct("4").getCost());
+
+
+        VendingMachine DrinksMachine = new VendingMachineHotDrink();
+        vendingMachine.addProduct(product5);
+        vendingMachine.addProduct(product6);
+        vendingMachine.addProduct(product7);
+        vendingMachine.addProduct(product8);
+        
+
+
+        System.out.println(vendingMachine.getProduct("4").getCost());
+        System.out.println(DrinksMachine.getProduct("5").getCost());
     }
 }
