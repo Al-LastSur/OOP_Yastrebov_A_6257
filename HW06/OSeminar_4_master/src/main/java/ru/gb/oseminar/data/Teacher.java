8e3cd34dad96d9f9ef845dec187d7c3c9fbaea4f
Alex.LastSurvivour <last.sur.90@gmail.com>
2024-06-29 09:39:26 +0500

    Added code from seminar04 to HW06 to opptimize

---
+++ HW06/OSeminar_4_master/src/main/java/ru/gb/oseminar/data/Teacher.java
@@ -0,0 +1,40 @@
+package ru.gb.oseminar.data;
+
+import java.time.LocalDate;
+
+
+public class Teacher extends User implements Comparable <Teacher>{
+
+    private Long teacherId;
+
+    public Teacher(String firstName, String secondName, String patronymic, LocalDate dateOfBirth) {
+        super(firstName, secondName, patronymic, dateOfBirth);
+    }
+    public Teacher(String firstName, String secondName, String patronymic, LocalDate dateOfBirth, Long teacherId) {
+        super(firstName, secondName, patronymic, dateOfBirth);
+        this.teacherId = teacherId;
+    }
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    @Override
+    public String toString(){
+        return "Teacher{" +
+               "teachertId='" + teacherId +'\'' +
+               ", firstName='" + super.getFirstName() + '\'' +
+               ", secondName='" + super.getSecondName() + '\'' +
+               ", patronymic='" + super.getPatronymic() + '\'' +
+               ", dateOfBirth=" + super.getDateOfBirth() +
+               '}';
+    }
+    @Override
+    public int compareTo(Teacher o){
+        return this.teacherId.compareTo(o.teacherId);
+    }
+}
